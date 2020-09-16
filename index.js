3fa72093b3962c4b8794bda13947fca8d2255b97
cwadeking <charles.w.king.cwk@gmail.com>
2020-09-15 20:14:28 -0500

    concluded validation and installed cors. currently testing api call with postman

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+var cors = require("cors");
 const repoContext = require("./repository/repository-wrapper.js");
+const validators = require("./validators/custom-validations.js");
 const express = require('express');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use((req, res, next) => validators.body(req, res, next));
 
 app.get("/", (req, res) => {
     res.send("hello");
